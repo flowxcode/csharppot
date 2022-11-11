19461eda872724be537f1260750fb6dbfbdba1e7
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-11-11 09:50:49 +0100

    enum selection tests

---
+++ TypesX/TypesX/ARConstants.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TypesX.FS
+{
+    [Serializable]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ARRef
+    {
+        ByShortRef = 0x00,
+        ByTagNumber = 0x20,
+        SpecialReference = 0x28,
+        AnyKeset = 0x30,
+        FreeAccess = 0x38,
+    }
+    [Serializable]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ARRefAll
+    {
+        ByShortRef = 0x00,
+        ByTagNumber = 0x20,
+        SpecialReference = 0x28,
+        AnyKeset = 0x30,
+        FreeAccess = 0x38,
+        All = 0x40,
+        XAll = ByShortRef | ByTagNumber | SpecialReference | AnyKeset | FreeAccess | All,
+    }
+}
---
+++ TypesX/TypesX/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TypesX.FS;
+
+Console.WriteLine("- sta x");
+
+var x = ARRef.ByShortRef;
+var y = ARRef.FreeAccess;
+var z = ARRefAll.All;
+
+Console.WriteLine("{0:X}", x);
+Console.WriteLine("{0:X}", y);
+Console.WriteLine("{0:X}", z);
+
+var values = Enum.GetValues(typeof(ARRef));
+
+Console.WriteLine("{0:X}", values);
+
+var values2 = Enum.GetValues(typeof(ARRefAll)).Cast<ARRefAll>();
+Console.WriteLine("{0:X}", values2);
+
+Console.WriteLine("- fine dev");
