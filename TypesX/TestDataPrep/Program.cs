2668b88572f4e29378dd41b07d0591b1b7dded64
Florian Isopp <florian.johannes.isopp@gmail.com>
2023-03-22 13:54:32 +0100

    yield output

--- TypesX/TestDataPrep/Program.cs
+++ TypesX/TestDataPrep/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestDataPrep;
+
 Console.WriteLine("pre sta");
 
-
+var x = new Tdate();
 
 Console.WriteLine("prep fin");
-
---
+++ TypesX/TestDataPrep/Tdate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataPrep
+{
+    internal class Tdate
+    {
+        public static IEnumerable<string> IFDTestData()
+        {
+
+            // SelectADF tests will run with keysets as following:
+            var keysetsWithSf1Adf = new Tuple<int, string>[]
+            {
+                new(11, "2"),
+                new(12, "3"),
+            };
+
+            foreach (var element in keysetsWithSf1Adf)
+            {
+                /* keyset SF1 refByTag */
+                yield return new string("1");
+                yield return new string("2");
+            }
+        }
+
+        public Tdate()
+        {
+            Console.WriteLine(IFDTestData());
+
+            Console.WriteLine("-");
+
+            foreach (var x in IFDTestData())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("-");
+
+            Console.WriteLine(IFDTestData);
+        }
+    }
+}
