086bd0b6b0970bc4f64b057a5bbdbfab0f52d6ad
Florian Isopp <florian.johannes.isopp@gmail.com>
2023-06-09 11:31:35 +0200

    byte variants testdata

---
+++ TypesX/ByteVariants/Byties.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteVariants
+{
+    public static class Byties
+    {
+        public static readonly IEnumerable<byte> ByteRange = Enumerable.Range(0, 256)
+            .Select(x => (byte)x);
+
+        private static readonly byte[] AcceptedCla =
+        {
+            0x00,
+            0x10,
+            0x0c,
+            0x10 | 0x0c,
+        };
+
+        public static int DutOS
+        {
+            get
+            {
+                var z = Environment.GetEnvironmentVariable("MAR_DUT");
+                return Environment.GetEnvironmentVariable("MAR_DUT") switch
+                {
+                    "JC" => 1,
+                    "RAW" => 2,
+                    _ => 2,
+                };
+            }
+        }
+
+        private static readonly int[] BitsToIgnoreIfSet = DutOS == 1
+            ? new int[] { 0, 1, 2, 3, 4, 6 }
+            : new int[] { 0, 1, 2, 3, 4 };
+
+        public static IEnumerable<byte> WrongClaTestDataX()
+        {
+            var x = ByteRange
+            .Where(v => IsValidValueToTest(v, BitsToIgnoreIfSet, AcceptedCla));
+            return x;
+        }
+
+        public static bool IsValidValueToTest(byte value, int[] bitsToCheck, byte[] acceptedValues)
+        {
+            var eval = !bitsToCheck.Any(bit => (value & (1 << bit)) != 0) &&
+                !acceptedValues.Any(classByte => value == classByte);
+
+            if (eval == false && bitsToCheck.Length == 6 && DutOS == 1)
+            {
+                return true;
+            }
+
+            return eval;
+        }
+    }
+}
---
+++ TypesX/ByteVariants/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ByteVariants;
+using System;
+using static System.Formats.Asn1.AsnWriter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello byte v");
+
+        var testdata = Byties.WrongClaTestDataX();
+
+        foreach (var item in testdata)
+        {
+            Console.Write(item.ToString("X"));
+            Console.Write(item.ToString(" "));
+            Console.Write(Convert.ToString(item, toBase: 2).PadLeft(8, '0'));
+            Console.Write(item.ToString("\n"));
+        }
+
+        Console.WriteLine("done");
+    }
+}
